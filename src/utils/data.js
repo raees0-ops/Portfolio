70bfb0e51abebd6742d4b7106ca1156b64b2c7b2
Raees <96441979+raees0-ops@users.noreply.github.com>
2025-03-19 17:48:45 +0530

    Update data.js

--- src/utils/data.js
+++ src/utils/data.js
@@ -22,7 +22,7 @@ skills: [
 },
 {
 title: "Databases",
-icon: "https://cdn3d.iconscout.com/3d/premium/thumb/database-storage-3d-icon-download-in-png-blend-fbx-gltf-file-formats-4703593.png",
+icon: "https://cdn3d.iconscout.com/3d/premium/thumb/data-3d-icon-download-in-png-blend-fbx-gltf-file-formats--storage-document-network-programming-and-development-pack-design-icons-7232746.png?f=webp",
 skills: [
 { skill: "SQL Server", percentage: "85%" },
 { skill: "MySQL", percentage: "80%" }
@@ -64,5 +64,29 @@ responsibilities: [
 "Delivered state-of-the-art user interface solutions using React.js, enhancing UI functionality.",
 "Developed progressive web applications with React, enhancing mobile site performance."
 ]
-}
+},{
+
+
+title: "Personal Project:  E-Commerce Website",
+    date: "None",
+    responsibilities: [
+      "Developed a full-stack e-commerce application using React.js for the frontend and Spring Boot for the backend",
+      "Implemented SQL Server for managing product inventory, user data, orders, and payments",
+      "Integrated JWT-based authentication and role-based access control for secure user management",
+      "Deployed the application using Docker and maintained version control with GitHub."
+    ]
+  },
+  {
+    title: "Personal Project:  Car Crash (2D Game) – Unity 3D",
+    date: "None",
+    responsibilities: [
+      "Developed a 2D car crash game in Unity 3D, utilizing C# and Unity’s physics system for realistic collisions",
+      "Designed and implemented vehicle movement, crash effects, and destruction mechanics for engaging gameplay",
+      "Created custom game assets in Adobe Photoshop, including cars, obstacles, UI elements, and backgrounds",
+      "Integrated an immersive sound system, adding engine sounds, collision effects, tire screeches,and background music to enhance the player experience",
+      "Optimized game performance for smooth frame rates on mobile platforms",
+       "Implemented UI elements such as score tracking, health system, and restart functionality for a complete game loop.",
+       "Worked with particle effects for debris and explosion effects during crashes",
+    ]
+  }
 ];
